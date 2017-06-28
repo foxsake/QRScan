183738952efc24be882c6e8adba8228a6a15b1c7
foxsake <paul.austian.alday@gmail.com>
2017-06-28 08:17:54 +0800

    initial commit

---
+++ app/src/main/java/alday/paa/qrscan/MainActivity.kt
@@ -0,0 +1,55 @@
+package alday.paa.qrscan
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import com.google.zxing.Result
+import me.dm7.barcodescanner.zxing.ZXingScannerView
+
+class MainActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
+    private var mScannerView: ZXingScannerView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mScannerView = ZXingScannerView(this)
+        setContentView(mScannerView)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+
+        if (id == R.id.action_settings) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mScannerView?.setResultHandler(this)
+        mScannerView?.startCamera()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mScannerView?.stopCamera()
+    }
+
+    override fun handleResult(result: Result) {
+        Log.v("QRScan", result.text)
+        Log.v("QRScan", result.barcodeFormat.toString())
+    }
+}
